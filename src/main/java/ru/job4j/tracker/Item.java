7800dc8aae2c7c0e6edbf7e70cf3c60eab806aac
redgad13 <vadim_kuzin@list.ru>
2022-11-20 12:27:42 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
 
@@ -32,4 +36,9 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
 }
