2d13be87e72cf0831f0256ba5bbb81ef65eb86cd
redgad13 <vadim_kuzin@list.ru>
2022-11-22 15:20:18 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,7 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(formatter));
+        Item item1 = new Item();
+        System.out.println(item1);
     }
 }
