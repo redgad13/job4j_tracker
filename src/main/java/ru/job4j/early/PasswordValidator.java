6b4f0861b24b45d429235b07077a1595142fd64a
redgad13 <vadim_kuzin@list.ru>
2022-12-03 09:42:02 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -17,16 +17,45 @@ public class PasswordValidator {
         if (!PasswordValidator.hasDigits(password)) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
+        if (!PasswordValidator.hasSpecial(password)) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
+        if (PasswordValidator.tooEasy(password)) {
+            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+        }
+
         return password;
     }
 
     public static boolean hasDigits(String password) {
-        boolean hasDigits = false;
-        for (int i = 0; i < password.length() && !hasDigits; i++) {
+        boolean isDigit = false;
+        for (int i = 0; i < password.length(); i++) {
             if (Character.isDigit(password.charAt(i))) {
-                hasDigits = true;
+                isDigit = true;
+                break;
             }
         }
-        return hasDigits;
+        return isDigit;
     }
+
+    public static boolean hasSpecial(String password) {
+        boolean isSpecial = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (password.charAt(i) >= 33 && password.charAt(i) <= 47
+                    || password.charAt(i) >= 58 && password.charAt(i) <= 64) {
+                isSpecial = true;
+                break;
+            }
+        }
+        return isSpecial;
+    }
+
+    public static boolean tooEasy(String password) {
+        return password.toLowerCase().contains("qwerty")
+                || password.toLowerCase().contains("12345")
+                || password.toLowerCase().contains("password")
+                || password.toLowerCase().contains("admin")
+                || password.toLowerCase().contains("user");
+    }
+
 }
