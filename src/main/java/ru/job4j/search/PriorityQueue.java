19d57620fdffb43b22bef82aa6f3cc8223c04570
redgad88 <vadim_kuzin@list.ru>
2022-12-07 11:13:01 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #368326]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,13 +8,13 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() >= element.getPriority()) {
-                index = tasks.size();
+            if (task.getPriority() < element.getPriority()) {
                 break;
             }
+            index++;
         }
         this.tasks.add(index, task);
-        }
+    }
 
     public Task take() {
         return tasks.poll();
