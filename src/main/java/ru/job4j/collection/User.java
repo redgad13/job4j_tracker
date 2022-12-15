2c303488af1e432f297dfd044707b1afd6ef33d5
redgad88 <vadim_kuzin@list.ru>
2022-12-15 16:21:32 +0300

    1. Организовать сортировку User [#10034 #370926]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -13,8 +13,9 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        if (this.name.compareTo(o.name) != 0) {
-            this.name.compareTo(o.name);
+        int i = this.name.compareTo(o.name);
+        if (i != 0) {
+           return i;
         }
         return Integer.compare(this.age, o.age);
     }
