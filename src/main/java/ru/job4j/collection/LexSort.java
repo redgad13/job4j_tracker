89a235e039dd87a72c8dad781935902f1eccba4e
redgad13 <vadim_kuzin@list.ru>
2022-12-17 15:15:44 +0300

    4. Сортировка номера [#221284 #372028]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -3,12 +3,12 @@ package ru.job4j.collection;
 import java.util.Comparator;
 import java.util.Objects;
 
-public class LexSort implements Comparator {
+public class LexSort implements Comparator<String> {
 
     @Override
-    public int compare(Object o1, Object o2) {
-        String[] o1numToArray = o1.toString().split(". ");
-        String[] o2numToArray = o2.toString().split(". ");
+    public int compare(String o1, String o2) {
+        String[] o1numToArray = o1.split(". ");
+        String[] o2numToArray = o2.split(". ");
         int o1Num = Integer.parseInt(o1numToArray[0]);
         int o2Num = Integer.parseInt(o2numToArray[0]);
         return Integer.compare(o1Num, o2Num);
