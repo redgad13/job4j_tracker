20af3715ec4d58d62edcdfdc9e18be54293567eb
redgad88 <vadim_kuzin@list.ru>
2022-12-22 18:19:09 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -8,7 +8,6 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                boolean isTrue = "".equals(start);
                 start = "".equals(start) ? start + el : start + "/" + el;
                 tmp.add(start);
             }
