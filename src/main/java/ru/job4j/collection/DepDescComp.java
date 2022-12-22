9ed648b374b020fc99398c8953d6ebde3aca9143
redgad13 <vadim_kuzin@list.ru>
2022-12-22 23:20:35 +0300

    2. Отсортировать департаменты [#34352 #373336]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,11 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result;
         String[] leftDeps = left.split("/");
         String[] rightDeps = right.split("/");
-        result = rightDeps[0].compareTo(leftDeps[0]);
-        result = result == 0 ? left.compareTo(right) : result;
-        return result;
+        int result = rightDeps[0].compareTo(leftDeps[0]);
+        return result == 0 ? left.compareTo(right) : result;
     }
 }
