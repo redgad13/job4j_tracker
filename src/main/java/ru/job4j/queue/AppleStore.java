82592305a0666eebc66d0e07a838f8a0c2dda013
redgad13 <vadim_kuzin@list.ru>
2022-12-22 23:17:06 +0300

    1. Интерфейс Queue [#504942 #373349]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,39 @@
+package ru.job4j.queue;
+
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        int customersCount = 0;
+        String name = null;
+        for (Customer customer : queue) {
+            customersCount++;
+            if (customersCount == count) {
+                name = customer.name();
+                break;
+            }
+        }
+        return name;
+    }
+
+    public String getLastUpsetCustomer() {
+        int customersCount = 0;
+        String name = null;
+        for (Customer customer : queue) {
+            customersCount++;
+            if (customersCount > count) {
+                name = customer.name();
+                break;
+            }
+        }
+        return name;
+    }
+}
