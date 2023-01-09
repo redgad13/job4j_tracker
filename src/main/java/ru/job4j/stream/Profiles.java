1cee7d419a9ad022d9cdf127349abb1dbc6e0bf0
redgad88 <vadim_kuzin@list.ru>
2023-01-09 11:34:03 +0300

    9. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,7 +1,6 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +10,5 @@ public class Profiles {
         return profiles.stream()
                 .map(Profile::getAddress)
                 .toList();
-    }
-
-    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream()
-                .map(Profile::getAddress)
-                .sorted(Comparator.comparing(Address::getCity))
-                .distinct()
-                .collect(Collectors.toList());
-
     }
 }
