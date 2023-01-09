67158019d02fe90c279f244ed0bfbd61b22b5d8a
redgad88 <vadim_kuzin@list.ru>
2023-01-09 11:31:21 +0300

    9. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/stream/checkstyle/Broken.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+
+    public String surname;
+
+    String name;
+
+    private int sizeOfEmpty = 10;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d) {
+
+    }
+}
