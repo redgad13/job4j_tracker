6da777f15014ed28ba52d10805cbff396b0ad603
redgad88 <vadim_kuzin@list.ru>
2023-01-11 13:16:01 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,52 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(p -> p.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.map(p -> new Tuple(p.name(), p.subjects()
+                        .stream()
+                        .mapToInt(Subject::score)
+                        .average()
+                        .orElse(0.0)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        Map<String, Double> subjects = stream.flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name, LinkedHashMap::new, Collectors.averagingDouble(Subject::score)));
+        return subjects.entrySet().stream()
+                .map(subj -> new Tuple(subj.getKey(), subj.getValue()))
+                .collect(Collectors.toList());
+
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(p -> new Tuple(p.name(), p.subjects()
+                        .stream()
+                        .mapToInt(Subject::score)
+                        .sum()))
+                .max((left, right) -> (int) (left.score() - right.score()))
+                .orElse(new Tuple("not found", 0.0));
+
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        Map<String, Double> subjects = stream.flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name, LinkedHashMap::new, Collectors.summingDouble(Subject::score)));
+        return subjects.entrySet().stream()
+                .map(s -> new Tuple(s.getKey(), s.getValue()))
+                .max((left, right) -> (int) (left.score() - right.score()))
+                .orElse(new Tuple("not found", 0.0));
+    }
+}
+
