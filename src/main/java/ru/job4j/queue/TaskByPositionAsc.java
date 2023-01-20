1948d3dc2f0f95662d47b79b973a4b9415ff89ad
redgad88 <vadim_kuzin@list.ru>
2023-01-20 11:34:01 +0300

    5. PriorityQueue [#504944 #382423]

--- src/main/java/ru/job4j/queue/TaskByPositionAsc.java
+++ src/main/java/ru/job4j/queue/TaskByPositionAsc.java
@@ -2,14 +2,10 @@ package ru.job4j.queue;
 
 import java.util.Comparator;
 
-public class TaskByPositionAsc implements Comparator<Position> {
+public class TaskByPositionAsc implements Comparator<Task> {
 
     @Override
-    public int compare(Position left, Position right) {
-        return left.compareTo(right);
-    }
-
-    public Comparator<Task> thenComparing(TaskByUrgencyDesc taskByUrgencyDesc) {
-        return taskByUrgencyDesc;
+    public int compare(Task o1, Task o2) {
+        return o1.position().compareTo(o2.position());
     }
 }
