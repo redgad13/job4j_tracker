a54c0221da0a3a5ce0f50b9426e5e2ab3f0c999d
redgad88 <vadim_kuzin@list.ru>
2023-07-10 20:30:03 +0300

    1. Реализация кеша на SoftReference [#1592 # [#1592 #428488]]

--- src/main/java/ru/job4j/cache/menu/Emulator.java
+++ src/main/java/ru/job4j/cache/menu/Emulator.java
@@ -2,9 +2,34 @@ package ru.job4j.cache.menu;
 
 import ru.job4j.cache.DirFileCache;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Emulator {
     public static void main(String[] args) {
-        DirFileCache dirFileCache = new DirFileCache(".\\data\\new_file.txt");
-        dirFileCache.get("test_text");
+        Emulator e = new Emulator();
+        e.putData();
+        DirFileCache dirFileCache = new DirFileCache(e.inputDirectory());
+        String s = dirFileCache.get("test");
+        System.out.println(s);
+
+    }
+
+    public String inputDirectory() {
+        System.out.println("enter directory");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public void putData() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(inputDirectory()))) {
+            System.out.println("Enter data");
+            Scanner sc = new Scanner(System.in);
+            bw.write(sc.nextLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
