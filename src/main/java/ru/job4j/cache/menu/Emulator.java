f5669a28dbccdce47a534b8b995c5015f35e166a
redgad88 <vadim_kuzin@list.ru>
2023-07-12 16:15:23 +0300

    1. Реализация кеша на SoftReference [#1592 # [#1592 #428488]]

--- src/main/java/ru/job4j/cache/menu/Emulator.java
+++ src/main/java/ru/job4j/cache/menu/Emulator.java
@@ -2,34 +2,52 @@ package ru.job4j.cache.menu;
 
 import ru.job4j.cache.DirFileCache;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Emulator {
     public static void main(String[] args) {
         Emulator e = new Emulator();
-        e.putData();
-        DirFileCache dirFileCache = new DirFileCache(e.inputDirectory());
-        String s = dirFileCache.get("test");
-        System.out.println(s);
-
+        System.out.println("Введите адрес каталога для кеширования");
+        String dir = e.inputDirectory();
+        System.out.println("Введите имя файла для кеширования или Exit для выхода");
+        String file = e.inputFileName();
+        while (!file.equals("exit")) {
+            file = dir.concat("/").concat(file);
+            DirFileCache dirFileCache = new DirFileCache(file);
+            dirFileCache.put(file, e.getData(file));
+            file = e.inputFileName();
+        }
     }
 
     public String inputDirectory() {
-        System.out.println("enter directory");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public String inputFileName() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     public void putData() {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(inputDirectory()))) {
-            System.out.println("Enter data");
             Scanner sc = new Scanner(System.in);
             bw.write(sc.nextLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getData(String path) {
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            while (br.ready()) {
+                rsl.append(br.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rsl.toString();
     }
 }
